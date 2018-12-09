5188044b92fc83a74872433405dab84c347513c5
Volodymyr Rudyi <volodymyr.rudyi@outlook.com>
2018-12-09 14:57:48 +0200

    Updates

---
+++ precache-manifest.e709f5c3970f64f97e3a96f0357b979f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "96e00db6278d966a85e2",
+    "url": "/address-book-app/static/js/runtime~main.96e00db6.js"
+  },
+  {
+    "revision": "213bd297fa42270b2d5a",
+    "url": "/address-book-app/static/js/main.213bd297.chunk.js"
+  },
+  {
+    "revision": "0603564ad81e10a6a1d2",
+    "url": "/address-book-app/static/js/1.0603564a.chunk.js"
+  },
+  {
+    "revision": "213bd297fa42270b2d5a",
+    "url": "/address-book-app/static/css/main.b933e1af.chunk.css"
+  },
+  {
+    "revision": "53a5e975e0ef9c386c2d2413bdfb861f",
+    "url": "/address-book-app/index.html"
+  }
+];
